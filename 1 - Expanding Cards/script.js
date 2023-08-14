4f57ff7104ce7440f956723e6ac37e560e1d4be1
itsjordanmuller <itsjordanmuller@gmail.com>
2023-08-13 21:21:42 -0700

    Set panels to active on when they are clicked

--- 1 - Expanding Cards/script.js
+++ 1 - Expanding Cards/script.js
@@ -1,3 +1,7 @@
 const panels = document.querySelectorAll(".panel");
 
-console.log(panels);
+panels.forEach((panel) => {
+  panel.addEventListener("click", () => {
+    panel.classList.add("active");
+  });
+});
