6605c15b4024b6c6c211cee02ab2771534782f81
itsjordanmuller <itsjordanmuller@gmail.com>
2023-08-19 16:28:28 -0700

    Remove appropriate classes and add hover classes as needed based on mouse position

--- 7 - Split Landing Page/script.js
+++ 7 - Split Landing Page/script.js
@@ -3,5 +3,11 @@ const right = document.querySelector(".right");
 const container = document.querySelector(".container");
 
 left.addEventListener("mouseenter", () => {
+  container.classList.remove("hover-right");
   container.classList.add("hover-left");
+});
+
+right.addEventListener("mouseenter", () => {
+  container.classList.remove("hover-left");
+  container.classList.add("hover-right");
 });
