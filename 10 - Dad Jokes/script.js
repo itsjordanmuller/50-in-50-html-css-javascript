abf4de199481fdebb6dbee1c266b5bddc678db14
itsjordanmuller <itsjordanmuller@gmail.com>
2023-08-22 18:07:27 -0700

    Define a function that generates a joke from the icanhazdadjoke API

--- 10 - Dad Jokes/script.js
+++ 10 - Dad Jokes/script.js
@@ -1,2 +1,16 @@
 const jokeElement = document.getElementById("joke");
 const jokeButton = document.getElementById("jokeBtn");
+
+generateJoke();
+
+function generateJoke() {
+  const config = {
+    headers: {
+      Accept: "application/json",
+    },
+  };
+
+  fetch("https://icanhazdadjoke.com/", config)
+    .then((res) => res.json())
+    .then((data) => console.log(data));
+}
