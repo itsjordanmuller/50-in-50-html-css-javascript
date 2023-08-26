afdd95daca3db372a7ab237cce4f53a390ac8a66
itsjordanmuller <itsjordanmuller@gmail.com>
2023-08-25 17:52:39 -0700

    Create an event listener on the text area to listne for a keyup and call the createTags function

--- 13 - Random Choice Picker/script.js
+++ 13 - Random Choice Picker/script.js
@@ -2,3 +2,7 @@ const tagsEl = document.getElementById("tags");
 const textarea = document.getElementById("textarea");
 
 textarea.focus();
+
+textarea.addEventListener("keyup", () => {
+  createTags(e.target.value);
+});
