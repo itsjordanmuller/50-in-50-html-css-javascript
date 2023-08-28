7dc6787c5820e9e3d5d7f204f1f23d3fb2f2af7f
itsjordanmuller <itsjordanmuller@gmail.com>
2023-08-27 18:27:44 -0700

    Update the counters by getting the attribute from data target and print to console

--- 15 - Incrementing Counter/script.js
+++ 15 - Incrementing Counter/script.js
@@ -2,4 +2,11 @@ const counters = document.querySelectorAll(".counter");
 
 counters.forEach((counter) => {
   counter.innerText = "0";
+
+  const updateCounter = () => {
+    const target = counter.getAttribute("data-target");
+    console.log(typeof target, target);
+  };
+
+  updateCounter();
 });
