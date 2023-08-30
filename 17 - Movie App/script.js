0ce1001860749561b40d1107944f2df71e4da469
itsjordanmuller <itsjordanmuller@gmail.com>
2023-08-29 18:54:09 -0700

    Define a function to color the ratings using classes, fix spacing error caused by extra div

--- 17 - Movie App/script.js
+++ 17 - Movie App/script.js
@@ -27,22 +27,31 @@ function showMovies(movies) {
     movieEl.classList.add("movie");
 
     movieEl.innerHTML = `
-    <div class="movie">
         <img
         src="${IMG_PATH + poster_path}"
         alt="${title}"
         />
         <div class="movie-info">
             <h3>${title}</h3>
-            <span class="green">${vote_average}</span>
+            <span class="${getClassByRate(vote_average)}">${vote_average}</span>
         </div>
         <div class="overview">
             <h3>Overview</h3>
             ${overview}
         </div>
-    </div>
     `;
+    main.appendChild(movieEl);
   });
+}
+
+function getClassByRate(vote) {
+  if (vote >= 8) {
+    return "green";
+  } else if (vote >= 5) {
+    return "orange";
+  } else {
+    return "red";
+  }
 }
 
 form.addEventListener("submit", (e) => {
