116dba45debdc37015617cf12fad8e9d5ae6faed
itsjordanmuller <itsjordanmuller@gmail.com>
2023-08-29 18:37:07 -0700

    Create a function to get the movie data and log it to console

--- 17 - Movie App/script.js
+++ 17 - Movie App/script.js
@@ -3,3 +3,12 @@ const API_URL =
 const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
 const SEARCH_URL =
   'https://api.themoviedb.org/3/search/movie?api_key=b74f37b931f19175ae0fb7af25f2d5c3&page=1&query="';
+
+getMovies(API_URL);
+
+async function getMovies(url) {
+  const res = await fetch(url);
+  const data = await res.json();
+
+  console.log(data.results);
+}
