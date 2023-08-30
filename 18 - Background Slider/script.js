fe3c79f4abb80761a24c50779ff7299798a7979f
itsjordanmuller <itsjordanmuller@gmail.com>
2023-08-30 14:08:14 -0700

    Set the active background slide by adding and removing the active class

--- 18 - Background Slider/script.js
+++ 18 - Background Slider/script.js
@@ -10,3 +10,11 @@ setBgToBody();
 function setBgToBody() {
   body.style.backgroundImage = slides[activeSlide].style.backgroundImage;
 }
+
+function setActiveSlide() {
+  slides.forEach((slide) => {
+    slide.classList.remove("active");
+  });
+
+  slides[activeSlide].classList.add("active");
+}
