5bd7b7c564923fc1e327a06715bcb414ae2d2bc1
itsjordanmuller <itsjordanmuller@gmail.com>
2023-08-29 18:35:16 -0700

    Create constant variables for all of the API urls we will need for movie list, search, and images

---
+++ 17 - Movie App/script.js
@@ -0,0 +1,5 @@
+const API_URL =
+  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=b74f37b931f19175ae0fb7af25f2d5c3&page=1";
+const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
+const SEARCH_URL =
+  'https://api.themoviedb.org/3/search/movie?api_key=b74f37b931f19175ae0fb7af25f2d5c3&page=1&query="';
