dd0a735b542a3603e3ab78e50f718a8791acafb2
itsjordanmuller <itsjordanmuller@gmail.com>
2023-08-31 16:00:34 -0700

    Add an AM or PM to the end of the time to show AM/PM

--- 19 - Theme Clock/script.js
+++ 19 - Theme Clock/script.js
@@ -49,6 +49,7 @@ function setTime() {
   const hoursForClock = hours % 12;
   const minutes = time.getMinutes();
   const seconds = time.getSeconds();
+  const ampm = hours >= 12 ? "PM" : "AM";
   //   console.log(time);
 
   hourEl.style.transform = `translate(-50%, -100%) rotate(${scale(
@@ -77,7 +78,7 @@ function setTime() {
 
   timeEl.innerHTML = `${hoursForClock}:${
     minutes < 10 ? `0${minutes}` : minutes
-  }`;
+  } ${ampm}`;
 }
 
 // StackOverflow https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
