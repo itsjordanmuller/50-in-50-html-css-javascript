ae71836671084bca8b62ffe60b4a2d8496bd75d4
itsjordanmuller <itsjordanmuller@gmail.com>
2023-08-31 15:53:51 -0700

    Use the scale function to map the hours to degrees and set the angle of the hour hand

--- 19 - Theme Clock/script.js
+++ 19 - Theme Clock/script.js
@@ -50,6 +50,19 @@ function setTime() {
   const minutes = time.getMinutes();
   const seconds = time.getSeconds();
   //   console.log(time);
+
+  hourEl.style.transform = `translate(-50%, -100%) rotate(${scale(
+    hoursForClock,
+    0,
+    11,
+    0,
+    360
+  )}deg)`;
 }
+
+// StackOverflow https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
+const scale = (num, in_min, in_max, out_min, out_max) => {
+  return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
+};
 
 setTime();
