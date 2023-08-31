22ff36aced3845b7e6eca4f12fb00191a9602998
itsjordanmuller <itsjordanmuller@gmail.com>
2023-08-31 15:55:33 -0700

    Set the rotation of the minutes and seconds hand and map them to 360 degrees

--- 19 - Theme Clock/script.js
+++ 19 - Theme Clock/script.js
@@ -58,6 +58,22 @@ function setTime() {
     0,
     360
   )}deg)`;
+
+  minuteEl.style.transform = `translate(-50%, -100%) rotate(${scale(
+    minutes,
+    0,
+    59,
+    0,
+    360
+  )}deg)`;
+
+  secondEl.style.transform = `translate(-50%, -100%) rotate(${scale(
+    seconds,
+    0,
+    59,
+    0,
+    360
+  )}deg)`;
 }
 
 // StackOverflow https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
