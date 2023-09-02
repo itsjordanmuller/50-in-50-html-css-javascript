0496c3306102fb19de71c7a31afac96755e2be06
itsjordanmuller <itsjordanmuller@gmail.com>
2023-09-01 17:31:01 -0700

    Add a span to the button coordinates each time the user clicks

--- 20 - Button Ripple Effect/script.js
+++ 20 - Button Ripple Effect/script.js
@@ -1,7 +1,7 @@
 const buttons = document.querySelectorAll(".ripple");
 
 buttons.forEach((button) => {
-  button.addEventListener("click", (e) => {
+  button.addEventListener("click", function (e) {
     const x = e.clientX;
     const y = e.clientY;
 
@@ -15,6 +15,13 @@ buttons.forEach((button) => {
     const xInside = x - buttonLeft;
     const yInside = y - buttonTop;
 
-    console.log(xInside, yInside);
+    // console.log(xInside, yInside);
+
+    const circle = document.createElement("span");
+    circle.classList.add("circle");
+    circle.style.top = yInside + "px";
+    circle.style.left = xInside + "px";
+
+    this.appendChild(circle);
   });
 });
