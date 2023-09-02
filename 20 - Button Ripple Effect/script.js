d6832dc0910bdcac3dc268a6a7c1b82a1437a3df
itsjordanmuller <itsjordanmuller@gmail.com>
2023-09-01 17:27:22 -0700

    Get the top and left offsets for the button position so we can calculate where the ripple should occur

--- 20 - Button Ripple Effect/script.js
+++ 20 - Button Ripple Effect/script.js
@@ -5,6 +5,10 @@ buttons.forEach((button) => {
     const x = e.clientX;
     const y = e.clientY;
 
-    console.log(x, y);
+    // console.log(x, y);
+    const buttonTop = e.target.offsetTop;
+    const buttonLeft = e.target.offsetLeft;
+
+    console.log(buttonTop, buttonLeft);
   });
 });
