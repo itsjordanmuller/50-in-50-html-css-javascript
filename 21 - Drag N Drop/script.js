00fee4e2e82f1dd44d17b0ebea3daa4922b3aba8
itsjordanmuller <itsjordanmuller@gmail.com>
2023-09-02 16:47:44 -0700

    Define functions for all of the different events that can happen when dragging and dropping

---
+++ 21 - Drag N Drop/script.js
@@ -0,0 +1,23 @@
+function dragStart() {
+  console.log("Drag Start");
+}
+
+function dragEnd() {
+  console.log("Drag End");
+}
+
+function dragOver() {
+  console.log("Drag Over");
+}
+
+function dragEnter() {
+  console.log("Drag Enter");
+}
+
+function dragLeave() {
+  console.log("Drag Leave");
+}
+
+function dragDrop() {
+  console.log("Drag Drop");
+}
