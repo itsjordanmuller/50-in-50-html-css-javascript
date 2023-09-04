cf19ef4e083d362bc4dac5ec2d2a40199908aa66
itsjordanmuller <itsjordanmuller@gmail.com>
2023-09-03 18:43:19 -0700

    Add an event listener to the clear button to clear the entire canvas

--- 22 - Drawing App/script.js
+++ 22 - Drawing App/script.js
@@ -87,5 +87,9 @@ decreaseBtn.addEventListener("click", () => {
 
 colorEl.addEventListener("change", (e) => (color = e.target.value));
 
+clearEl.addEventListener("click", () =>
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
+);
+
 // drawCircle(100, 200);
 // drawLine(300, 300, 500, 500);
