efa5a458e79a3f4259a614b89c7bb7a8704c6b72
itsjordanmuller <itsjordanmuller@gmail.com>
2023-09-03 18:21:38 -0700

    Create variables for x and y that we can set later on

--- 22 - Drawing App/script.js
+++ 22 - Drawing App/script.js
@@ -3,6 +3,8 @@ const ctx = canvas.getContext("2d");
 
 let size = 20;
 let color = "black";
+let x;
+let y;
 
 function drawCircle(x, y) {
   ctx.beginPath();
@@ -20,5 +22,5 @@ function drawLine(x1, y1, x2, y2) {
   ctx.stroke();
 }
 
-drawCircle(100, 200);
-drawLine(300, 300, 500, 500);
+// drawCircle(100, 200);
+// drawLine(300, 300, 500, 500);
