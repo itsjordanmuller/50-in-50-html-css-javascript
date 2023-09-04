c083bbc5c61c8d0c63203d5642825a2d17bb9dad
itsjordanmuller <itsjordanmuller@gmail.com>
2023-09-03 18:17:35 -0700

    Begin to create a function to draw circles using canvas API

--- 22 - Drawing App/script.js
+++ 22 - Drawing App/script.js
@@ -1,2 +1,9 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
+
+let size = 20;
+
+function drawCircle(x, y) {
+  ctx.beginPath();
+  ctx.arc(x, y, size, 0, Math.PI * 2);
+}
