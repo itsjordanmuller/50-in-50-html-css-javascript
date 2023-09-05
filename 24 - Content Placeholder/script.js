b77ad946b946e7615a9707fc94c11eeafce4c7a2
itsjordanmuller <itsjordanmuller@gmail.com>
2023-09-05 15:54:08 -0700

    Set a timeout of 2.5 seconds, insert the content and remove the animation after that time using the function

--- 24 - Content Placeholder/script.js
+++ 24 - Content Placeholder/script.js
@@ -8,6 +8,8 @@ const date = document.getElementById("date");
 const animated_bgs = document.querySelectorAll(".animated-bg");
 const animated_bg_texts = document.querySelectorAll(".animated-bg-text");
 
+setTimeout(getData, 2500);
+
 function getData() {
   header.innerHTML =
     '<img src="https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2102&q=80" alt="" />';
