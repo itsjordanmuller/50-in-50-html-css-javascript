e1d1bedd281ce163014c028b30c02fa825ebe054
itsjordanmuller <itsjordanmuller@gmail.com>
2023-09-05 15:52:57 -0700

    Remove the placeholder data from the HTML and use JavaScript to insert it into the HTML using a function

--- 24 - Content Placeholder/script.js
+++ 24 - Content Placeholder/script.js
@@ -2,8 +2,20 @@ const header = document.getElementById("header");
 const title = document.getElementById("title");
 const excerpt = document.getElementById("excerpt");
 const profile_img = document.getElementById("profile_img");
-const name = document.getElementById("name");
+const author_name = document.getElementById("name");
 const date = document.getElementById("date");
 
 const animated_bgs = document.querySelectorAll(".animated-bg");
 const animated_bg_texts = document.querySelectorAll(".animated-bg-text");
+
+function getData() {
+  header.innerHTML =
+    '<img src="https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2102&q=80" alt="" />';
+  title.innerHTML = "Lorem ipsum dolor sit amet";
+  excerpt.innerHTML =
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore perferendis";
+  profile_img.innerHTML =
+    '<img src="https://randomuser.me/api/portraits/men/45.jpg" alt="Author Portrait" />';
+  author_name.innerHTML = "John Doe";
+  date.innerHTML = "Sep 5, 2023";
+}
