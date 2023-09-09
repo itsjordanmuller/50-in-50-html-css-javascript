25b773716a9e46724f9cdaa5c077534985be38b3
itsjordanmuller <itsjordanmuller@gmail.com>
2023-09-08 18:06:46 -0700

    Add different notification types, style the color of each differently, and randomly generate the type and message when each notification is created

--- 27 - Toast Notifications/script.js
+++ 27 - Toast Notifications/script.js
@@ -8,11 +8,15 @@ const messages = [
   "Message Four",
 ];
 
+const types = ["info", "success", "error"];
+
 button.addEventListener("click", () => createNotification());
 
-function createNotification() {
+function createNotification(message = null, type = null) {
   const notification = document.createElement("div");
   notification.classList.add("toast");
+
+  notification.classList.add(type ? type : getRandomType());
 
   notification.innerText = getRandomMessage();
 
@@ -25,4 +29,8 @@ function createNotification() {
 
 function getRandomMessage() {
   return messages[Math.floor(Math.random() * messages.length)];
+}
+
+function getRandomType() {
+  return types[Math.floor(Math.random() * types.length)];
 }
