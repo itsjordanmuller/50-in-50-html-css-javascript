6cf536dc78185f415397cde7062433bf032fc597
itsjordanmuller <itsjordanmuller@gmail.com>
2023-09-10 15:23:00 -0700

    Create a custom double click function by checking the difference between click times

--- 029 - Double Heart Click/script.js
+++ 029 - Double Heart Click/script.js
@@ -1,6 +1,21 @@
 const loveMe = document.querySelector(".loveMe");
 const times = document.querySelector("#times");
 
-loveMe.addEventListener("dblclick", (e) => {
-  console.log(123);
+let clickTime = 0;
+
+loveMe.addEventListener("click", (e) => {
+  if (clickTime === 0) {
+    clickTime = new Date().getTime();
+    // console.log(clickTime);
+  } else {
+    if (new Date().getTime() - clickTime < 800) {
+      console.log(123);
+    } else {
+      clickTime = new Date().getTime();
+    }
+  }
 });
+
+// loveMe.addEventListener("dblclick", (e) => {
+//   console.log(123);
+// });
