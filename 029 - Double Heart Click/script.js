9f19c00624c263923a3b4c2d643ad5fc51950591
itsjordanmuller <itsjordanmuller@gmail.com>
2023-09-10 15:30:25 -0700

    Create a heart at the coordinates clicked on the picture that uses the heart animation

--- 029 - Double Heart Click/script.js
+++ 029 - Double Heart Click/script.js
@@ -9,12 +9,34 @@ loveMe.addEventListener("click", (e) => {
     // console.log(clickTime);
   } else {
     if (new Date().getTime() - clickTime < 800) {
-      console.log(123);
+      createHeart(e);
+      clickTime = 0;
     } else {
       clickTime = new Date().getTime();
     }
   }
 });
+
+const createHeart = (e) => {
+  const heart = document.createElement("i");
+  heart.classList.add("fas");
+  heart.classList.add("fa-heart");
+
+  const x = e.clientX;
+  const y = e.clientY;
+
+  const offsetLeft = e.target.offsetLeft;
+  const offsetTop = e.target.offsetTop;
+
+  const xInside = x - offsetLeft;
+  const yInside = y - offsetTop;
+
+  heart.style.top = `${yInside}px`;
+  heart.style.left = `${xInside}px`;
+
+  loveMe.appendChild(heart);
+  //   console.log(xInside, yInside);
+};
 
 // loveMe.addEventListener("dblclick", (e) => {
 //   console.log(123);
