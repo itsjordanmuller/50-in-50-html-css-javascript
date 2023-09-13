acea6a2ba0c0f019cfde7d4d9be9b0f9f9511b4d
itsjordanmuller <itsjordanmuller@gmail.com>
2023-09-12 17:06:18 -0700

    Define a function that gets a random lowercase letter from a range of character codes

--- 031 - Password Generator/script.js
+++ 031 - Password Generator/script.js
@@ -2,4 +2,8 @@ function getRandomUpper() {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
 }
 
-console.log(getRandomUpper());
+function getRandomLower() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+console.log(getRandomUpper(), getRandomLower());
