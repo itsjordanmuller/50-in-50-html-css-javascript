9fe1bc6990931514f47ce789f63091fbd83f8c48
itsjordanmuller <itsjordanmuller@gmail.com>
2023-09-13 14:37:36 -0700

    Add an event listener that will check the options each time one is clicked

--- 032 - Good, Cheap, Fast Checkboxes/script.js
+++ 032 - Good, Cheap, Fast Checkboxes/script.js
@@ -3,6 +3,10 @@ const good = document.querySelector("#good");
 const cheap = document.querySelector("#cheap");
 const fast = document.querySelector("#fast");
 
+toggles.forEach((toggle) =>
+  toggle.addEventListener("change", (e) => checkOptions(e.target))
+);
+
 function checkOptions(theClickedOne) {
   if (good.checked && cheap.checked && fast.checked) {
     if (good === theClickedOne) {
