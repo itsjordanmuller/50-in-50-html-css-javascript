158b8aaae1de2c2a9531f0df38fc58bbef28d843
itsjordanmuller <itsjordanmuller@gmail.com>
2023-09-17 16:30:58 -0700

    Define a function to get a random color from the list of colors

--- 036 - Hoverboard/script.js
+++ 036 - Hoverboard/script.js
@@ -3,3 +3,7 @@ const container = document.getElementById("container");
 const colors = ["#e74c3c", "#8e44ad", "#3498db", "#e67e22", "#2ecc71"];
 
 const SQUARES = 500;
+
+function getRandomColor() {
+  return colors[Math.floor(Math.random() * colors.length)];
+}
