80e798a90dfdb5f343c0d8ef770f7df475d0fa16
itsjordanmuller <itsjordanmuller@gmail.com>
2023-09-16 20:44:57 -0700

    Define a function to iterate the index and change the image based on an interval

--- 035 - Image Carousel/script.js
+++ 035 - Image Carousel/script.js
@@ -6,6 +6,13 @@ const img = document.querySelectorAll("#imgs img");
 
 let idx = 0;
 
+let interval = setInterval(run, 3000);
+
+function run() {
+  idx++;
+  changeImage();
+}
+
 function changeImage() {
   if (idx > img.length - 1) {
     idx = 0;
