610274db3a73b7cdcbcf9af7938761ded650d04d
itsjordanmuller <itsjordanmuller@gmail.com>
2023-09-18 13:28:08 -0700

    Define and call a function to fetch all pokemon up to the count

--- 037 - Pokedex/script.js
+++ 037 - Pokedex/script.js
@@ -19,9 +19,17 @@ const colors = {
   normal: "#f5f5f5",
 };
 
+const fetchPokemons = async () => {
+  for (let i = 1; i <= pokemon_count; i++) {
+    await getPokemon(i);
+  }
+};
+
 const getPokemon = async (id) => {
   const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
   const res = await fetch(url);
   const data = await res.json();
   console.log(data);
 };
+
+fetchPokemons();
