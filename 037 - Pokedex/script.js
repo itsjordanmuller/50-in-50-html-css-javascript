1daa4bde600e8a5f51e59ca27acce37dd5d7a723
itsjordanmuller <itsjordanmuller@gmail.com>
2023-09-18 13:23:54 -0700

    Create a dictionary full of types as keys and their colors as values

--- 037 - Pokedex/script.js
+++ 037 - Pokedex/script.js
@@ -1,3 +1,20 @@
 const pokeContainer = document.getElementById("poke-container");
 
 const pokemon_count = 150;
+
+const colors = {
+  fire: "#fddfdf",
+  grass: "#defde0",
+  electric: "#fcf7de",
+  water: "#def3fd",
+  ground: "#f4e7da",
+  rock: "#d5d5d4",
+  fairy: "#fceaff",
+  poison: "#98d7a5",
+  bug: "#f8d5a3",
+  dragon: "#97b3e6",
+  psychic: "#eaeda1",
+  flying: "#f5f5f5",
+  fighting: "#e6e0d4",
+  normal: "#f5f5f5",
+};
