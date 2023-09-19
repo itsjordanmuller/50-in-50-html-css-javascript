4f3d35f216f71b9c063aab2ef37864be20dfba78
itsjordanmuller <itsjordanmuller@gmail.com>
2023-09-19 15:20:41 -0700

    Define a function to hide all items by removing active class

--- 038 - Mobile Tab Navigation/script.js
+++ 038 - Mobile Tab Navigation/script.js
@@ -4,3 +4,7 @@ const listItems = document.querySelectorAll("nav ul li");
 function hideAllContents() {
   contents.forEach((content) => content.classList.remove("show"));
 }
+
+function hideAllItems() {
+  listItems.forEach((item) => item.classList.remove("active"));
+}
