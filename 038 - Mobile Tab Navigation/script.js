82c18c79b226ce82425db6e4df463cfef330f4fe
itsjordanmuller <itsjordanmuller@gmail.com>
2023-09-19 15:20:08 -0700

    Define a function to hide all contents by removing show class

--- 038 - Mobile Tab Navigation/script.js
+++ 038 - Mobile Tab Navigation/script.js
@@ -1,2 +1,6 @@
 const contents = document.querySelectorAll(".content");
 const listItems = document.querySelectorAll("nav ul li");
+
+function hideAllContents() {
+  contents.forEach((content) => content.classList.remove("show"));
+}
