d42bcaa66de61373d276314455279529baae66ee
itsjordanmuller <itsjordanmuller@gmail.com>
2023-09-23 14:21:54 -0700

    Get data from the random user API, log it to the console

--- 042 - Live User Filter/script.js
+++ 042 - Live User Filter/script.js
@@ -1,2 +1,14 @@
 const result = document.getElementById("result");
 const filter = document.getElementById("filter");
+
+const listItems = [];
+
+getData();
+
+async function getData() {
+  const res = await fetch("https://randomuser.me/api/?results=50");
+
+  const data = await res.json();
+
+  console.log(data);
+}
