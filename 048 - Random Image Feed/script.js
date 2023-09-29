5ea6f0014802662b0b5918e9ba6b555e5950233d
itsjordanmuller <itsjordanmuller@gmail.com>
2023-09-29 16:21:54 -0700

    Define a function to get a random number for the image sizes

--- 048 - Random Image Feed/script.js
+++ 048 - Random Image Feed/script.js
@@ -8,3 +8,7 @@ for (let i = 0; i < row * 3; i++) {
   const img = document.createElement("img");
   img.src = `${unsplashURL}`;
 }
+
+function getRandomNum() {
+  return Math.floor(Math.random() * 10) + 300;
+}
