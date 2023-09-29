a687696ee2e7c40b281b03a9d422cc78e01f1bad
itsjordanmuller <itsjordanmuller@gmail.com>
2023-09-29 16:20:38 -0700

    Create a loop that will create 30 images using the unsplash URL

--- 048 - Random Image Feed/script.js
+++ 048 - Random Image Feed/script.js
@@ -3,3 +3,8 @@ const container = document.querySelector(".container");
 const unsplashURL = "https://source.unsplash.com/random/";
 
 const rows = 10;
+
+for (let i = 0; i < row * 3; i++) {
+  const img = document.createElement("img");
+  img.src = `${unsplashURL}`;
+}
