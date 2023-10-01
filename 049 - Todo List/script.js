5e730bc3abd6d7defbafb1617e516f5b90c98803
itsjordanmuller <itsjordanmuller@gmail.com>
2023-09-30 19:00:55 -0700

    Define a function to add a todo to the list, start by getting and logging text to console, call when form is submitted

--- 049 - Todo List/script.js
+++ 049 - Todo List/script.js
@@ -4,4 +4,16 @@ const todosUL = document.getElementById("todos");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
+
+  addTodo();
 });
+
+function addTodo(todo) {
+  let todoText = input.value;
+
+  if (todo) {
+    todoText = todoText;
+  }
+
+  console.log(todoText);
+}
