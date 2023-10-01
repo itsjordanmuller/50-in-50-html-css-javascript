0786c066c936ecf7aa539c89861ed611472a2ee2
itsjordanmuller <itsjordanmuller@gmail.com>
2023-09-30 19:16:34 -0700

    Define a function to update the local storage with todos, and load todos from storage

--- 049 - Todo List/script.js
+++ 049 - Todo List/script.js
@@ -4,6 +4,10 @@ const todosUL = document.getElementById("todos");
 
 const todos = JSON.parse(localStorage.getItem("todos"));
 
+if (todos) {
+  todos.forEach((todo) => addTodo(todo));
+}
+
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 
@@ -14,7 +18,7 @@ function addTodo(todo) {
   let todoText = input.value;
 
   if (todo) {
-    todoText = todoText;
+    todoText = todo.text;
   }
 
   if (todoText) {
@@ -37,5 +41,22 @@ function addTodo(todo) {
     todosUL.appendChild(todoEl);
 
     input.value = "";
+
+    updateLS();
   }
+}
+
+function updateLS() {
+  todosEl = document.querySelectorAll("li");
+
+  const todos = [];
+
+  todosEl.forEach((todoEl) => {
+    todos.push({
+      text: todoEl.innerText,
+      completed: todoEl.classList.contains("completed"),
+    });
+  });
+
+  localStorage.setItem("todos", JSON.stringify(todos));
 }
