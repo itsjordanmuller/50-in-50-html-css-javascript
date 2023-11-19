21154e97eb205b3675606581aa8f11595f885ae6
itsjordanmuller <itsjordanmuller@gmail.com>
2023-11-18 17:31:09 -0800

    Modify Scroll Animation scrip to cycle color array

--- 006-Scroll-Animation/script.js
+++ 006-Scroll-Animation/script.js
@@ -1,14 +1,25 @@
 const boxes = document.querySelectorAll(".box");
+const colors = [
+  "#ffadad",
+  "#ffd6a5",
+  "#fdffb6",
+  "#caffbf",
+  "#9bf6ff",
+  "#a0c4ff",
+  "#bdb2ff",
+  "#ffc6ff",
+];
 
 window.addEventListener("scroll", checkBoxes);
-
 checkBoxes();
 
 function checkBoxes() {
   const triggerBottom = (window.innerHeight / 5) * 4;
 
-  boxes.forEach((box) => {
+  boxes.forEach((box, index) => {
     const boxTop = box.getBoundingClientRect().top;
+
+    box.style.backgroundColor = colors[index % colors.length];
 
     if (boxTop < triggerBottom) {
       box.classList.add("show");
